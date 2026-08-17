310424e3be87722b04434ddcdc9f2dbca9d42789
FelipeZuquettiPereira <felipe_z_pereira@estudante.sesisenai.org.br>
2026-08-17 11:11:48 -0300

    add - Script

--- database/db.sql
+++ database/db.sql
@@ -1,10 +1,16 @@
 
-CREATE DATABASE livraria_icaro_m2;
-USE livraria_icaro_m2;
+CREATE DATABASE sistema_pratos;
+USE sistema_pratos;
 
-CREATE TABLE livros (
+CREATE TABLE usuario (
+    nome VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL
+ )
+
+CREATE TABLE pratos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(200) NOT NULL,
-    autor VARCHAR(100)NOT NULL,
-    ano INT NOT NULL
+    nome VARCHAR(200) NOT NULL,
+    descicao VARCHAR(100)NOT NULL,
+    preco FLOAT NOT NULL,
+    categoria VARCHAR(200) NOT NULL
 );
